6ea4b2fb165e00ad593b7e2ba073b0cda3ce38ec
PulengKgatlhane <pulengkgatlhane1973@gmail.com>
2023-03-22 13:03:10 -0700

    0x03. C - Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,35 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
-
-/**
- * main - Determines if a number is positive, negative or zero.
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int n;
-
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-	{
-		printf("%d is positive\n", n);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
-		printf("%d is negative\n", n);
-	}
-	return (0);
-}
 void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
+#endif
 
-
-#endif /* MAIN_H */
